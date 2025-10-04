f3d54fb6a40621436420ff657b6592b4bbd6e7f5
Daniel Kiss <littlesnorrboy@gmail.com>
2025-10-04 20:57:46 +0200

    Add an example program

--- src/editor.rs
+++ src/editor.rs
@@ -2,19 +2,25 @@ use std::{cell::RefCell, rc::Rc};
 
 use dioxus::prelude::*;
 
+const FIB_PROG: &[u8] = include_bytes!("../assets/fibonacci_program_recursive.yaml");
+
 #[derive(Clone)]
 pub struct CaoLangProgram(pub Rc<RefCell<cao_lang::prelude::CaoProgram>>);
 
 #[component]
 pub fn Editor() -> Element {
-    let program = use_hook(|| CaoLangProgram(Default::default()));
+    let program = use_signal(|| {
+        let program: cao_lang::prelude::CaoProgram = serde_yaml::from_slice(&FIB_PROG).unwrap();
+        CaoLangProgram(Rc::new(RefCell::new(program)))
+    });
 
-    let json = format!("{:?}", program.0.borrow());
+    let json = format!("{:#?}", program.read().0.borrow());
     rsx! {
         div {
             "hello"
-        {json}
-
+            pre {
+                {json}
+            }
         }
     }
 }
