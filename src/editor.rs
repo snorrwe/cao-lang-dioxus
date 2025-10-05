453333e73ea0c0ae72a58bcb626a2684caa83cca
Daniel Kiss <littlesnorrboy@gmail.com>
2025-10-05 15:02:39 +0200

    f

--- src/editor.rs
+++ src/editor.rs
@@ -139,7 +139,7 @@ fn FunctionName(function_idx: usize, name: String) -> Element {
         h2 { class: "text-2xl", {name} }
         input {
             r#type: "text",
-            class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+            class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
             value: "{name}",
             oninput: move |event| name.set(event.value()),
         }
