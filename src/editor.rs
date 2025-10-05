7fa6eaff43dc1432a6c0d403817dc1387f93b02e
Daniel Kiss <littlesnorrboy@gmail.com>
2025-10-05 15:00:18 +0200

    Render readvar vard

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use cao_lang::prelude::CardIndex;
+use cao_lang::prelude::{Card as CaoLangCard, CardIndex};
 use dioxus::prelude::*;
 
 const FIB_PROG: &[u8] = include_bytes!("../assets/fibonacci_program_recursive.yaml");
@@ -68,28 +68,61 @@ fn FunctionBody(function_idx: usize) -> Element {
         }
     });
     rsx! {
-        ul { {cards} }
+        ul { class: "ml-4", {cards} }
     }
 }
 
 #[component]
 fn Card(idx: CardIndex) -> Element {
-    let program_sig = use_context::<Signal<CaoLangProgram>>();
+    let mut program_sig = use_context::<Signal<CaoLangProgram>>();
     let program = &program_sig.read().0;
     let card = program.get_card(&idx).unwrap();
 
-    let children = (0..card.num_children() as usize).map(|i| {
-        let idx = idx.clone().with_sub_index(i);
-        rsx! {
-            li {
-                Card { idx }
+    let children = (0..card.num_children() as usize).map({
+        let idx = idx.clone();
+        move |i| {
+            let idx = idx.clone().with_sub_index(i);
+            rsx! {
+                li {
+                    Card { idx }
+                }
             }
         }
     });
 
+    let body = match card {
+        CaoLangCard::ReadVar(name) => {
+            let mut name = use_signal(|| name.clone());
+            use_effect(move || {
+                let mut program = program_sig.write();
+                let card = program.0.get_card_mut(&idx).unwrap();
+                if let CaoLangCard::ReadVar(card_name) = card {
+                    *card_name = name.read().to_string();
+                }
+            });
+
+            rsx! {
+                div { class: "flex justify-start gap-4 w-full items-center ",
+                    h3 { class: "text-xl flex-none", {card.name()} }
+                    input {
+                        r#type: "text",
+                        class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                        value: "{name}",
+                        oninput: move |event| name.set(event.value()),
+                    }
+                }
+            }
+        }
+        _ => {
+            rsx!(
+                h3 { class: "text-xl", {card.name()} }
+            )
+        }
+    };
+
     rsx! {
         div {
-            h3 { class: "text-xl", {card.name()} }
+            div { {body} }
             ul { class: "ml-4 mt-2", {children} }
         }
     }
