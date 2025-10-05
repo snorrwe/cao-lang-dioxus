a7b4a950fe4c5e4c9eca668dc9aa9b93d7935f87
Daniel Kiss <littlesnorrboy@gmail.com>
2025-10-05 14:17:35 +0200

    Remove unused svg

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ enum Route {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
